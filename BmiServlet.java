9b7075624d4a2eeb597198f355c6b0d4b23e4077
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2020-11-16 15:41:58 +0800

    Add files via upload

--- BmiServlet.java
+++ BmiServlet.java
@@ -2,39 +2,40 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.rmi.dgc.VMID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-public class BmiServlet extends HttpServlet{
+public class BmiServlet extends HttpServlet {
+	private double min;
+	private double max;
+	//获取初始化参数
 	@Override
-	protected void service(HttpServletRequest request,HttpServletResponse response) throws  ServletException, IOException{
-		//request提供获取参数的方法geParmenter方法
-		String weight = request.getParameter("weight");
-		String height = request.getParameter("height");
-		System.out.println(weight+","+height);
-		//对于多选框,如果用户一个都不选,会返回null,当有多个请求参数名相同,应该使用geParamentValues()方法返回String[]数组
-		String[] interset = request.getParameterValues("interset");
-		if (interset != null) {
-			for(String string : interset)
-				System.out.println(string);
-		}
+	public void init(){
+		String strMin = getInitParameter("min");
+		String strMax = getInitParameter("max");
+		min = Double.parseDouble(strMin);
+		max = Double.parseDouble(strMax);
+	}
+	
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		double height = Double.parseDouble(request.getParameter("height"));
+		double weight = Double.parseDouble(request.getParameter("weight"));
 		
-		//计算bmi指数
-		double bmi = Double.parseDouble(weight) / (Double.parseDouble(height) * Double.parseDouble(height));
+		double bmi = weight / (height * height);
+		
 		String state = "正常";
-		if (bmi < 19) 
+		if (bmi < min) {
 			state = "过轻";
-		if(bmi > 25)
+		}
+		if (bmi > max) {
 			state = "过重";
-		
-		/*这两行代码有两个作用:
-		 * 1.设置content_type消息头,告诉浏览器服务器返回的数据类型以编码格式
-		 * 2.设置out在输出时,使用哪种字符集来编码*/
+		}
 		response.setContentType("text/html:charset = utf-8");
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println(bmi);
