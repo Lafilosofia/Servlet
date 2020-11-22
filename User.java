880fe8013c80f3c21664e5b0f23b4bb1389f958f
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2020-11-22 09:33:07 +0800

    Add files via upload

--- User.java
+++ User.java
@@ -5,6 +5,32 @@ import java.io.Serializable;
 public class User implements Serializable{
 	private String userName;
 	private int age;
-	public 
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", age=" + age + "]";
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public User(String userName, int age) {
+		super();
+		this.userName = userName;
+		this.age = age;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
 	
 }
